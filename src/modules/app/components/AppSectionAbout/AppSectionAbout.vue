372383003196198a4ac2705053d38fe232807bdf
Pavel Spacil <pavel.spacil@gmail.com>
2021-09-22 13:08:30 +0200

    Add `AppAboutCardList` skeleton to the `AppSectionAbout` component

--- src/modules/app/components/AppSectionAbout/AppSectionAbout.vue
+++ src/modules/app/components/AppSectionAbout/AppSectionAbout.vue
@@ -1,13 +1,17 @@
 <template>
     <section class="appsectionabout container">
         <h2 class="h1 tea-center">{{ $t('appsectionabout.title') }}</h2>
-        AppAboutCardList
+        <app-about-card-list />
     </section>
 </template>
 
 <script>
+import AppAboutCardList from '@/modules/app/components/AppAboutCardList/AppAboutCardList.vue';
+
 export default {
     name: 'AppSectionAbout',
+
+    components: { AppAboutCardList },
 };
 </script>
 
