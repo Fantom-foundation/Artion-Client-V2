8467e5fcab95d34b41d699e29f4aecc531f81fef
Pavel Spacil <pavel.spacil@gmail.com>
2021-09-23 08:01:15 +0200

    Create categories constant

---
+++ src/common/constants/categories.js
@@ -0,0 +1,48 @@
+/**
+ * Category object
+ * @typedef {Object} Category
+ * @property {number} id
+ * @property {string} icon Name of the icon from AppIconset
+ * @property {string} label Translation code
+ */
+
+/**
+ * @type {Category[]}
+ */
+export const CATEGORIES = [
+    {
+        id: 0,
+        icon: 'rainbow',
+        label: 'category.art',
+    },
+    {
+        id: 1,
+        icon: 'bear',
+        label: 'category.collectibles',
+    },
+    {
+        id: 2,
+        icon: 'soccerball',
+        label: 'category.sports',
+    },
+    {
+        id: 3,
+        icon: 'tools',
+        label: 'category.utility',
+    },
+    {
+        id: 4,
+        icon: 'cardboard',
+        label: 'category.trading_cards',
+    },
+    {
+        id: 5,
+        icon: 'monster',
+        label: 'category.virtual_worlds',
+    },
+    {
+        id: 6,
+        icon: 'domain',
+        label: 'category.domain_names',
+    },
+];
