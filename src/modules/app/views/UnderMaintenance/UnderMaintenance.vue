3a3aaba376e80279cb1d3025a91f02f59cdaece1
sidevshiy <evgeni_lyax7@mail.ru>
2021-09-23 05:40:55 +0200

    UnderMaintenance page

--- src/modules/app/views/UnderMaintenance/UnderMaintenance.vue
+++ src/modules/app/views/UnderMaintenance/UnderMaintenance.vue
@@ -1,5 +1,17 @@
 <template>
-    <div class="undermaintenance">UnderMaintenance</div>
+    <div class="undermaintenance">
+        <div class="undermaintenance_wrap container">
+            <div class="undermaintenance_box">
+                <div class="undermaintenance_col undermaintenance_col-mod">
+                    <f-image src="img/undermaintenance.png" class="undermaintenance_img" alt="man" />
+                </div>
+                <div class="undermaintenance_col">
+                    <h1>{{ $t('undermaintenance.text') }}</h1>
+                    <router-link :to="{ name: 'main' }" class="btn btn-lg">{{$t('undermaintenance.link_title')}}</router-link> 
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
