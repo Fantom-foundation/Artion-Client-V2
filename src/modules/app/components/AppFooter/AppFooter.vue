a5da69a941ad7631d3a5b17ff0d2d12d81bc62d2
sidevshiy <evgeni_lyax7@mail.ru>
2021-09-23 17:05:59 +0200

    Content for AppFooter component

--- src/modules/app/components/AppFooter/AppFooter.vue
+++ src/modules/app/components/AppFooter/AppFooter.vue
@@ -1,13 +1,37 @@
 <template>
-    <div class="appfooter">AppFooter</div>
+    <div class="appfooter">
+        <div class="appfooter_item">
+            <router-link to="/"><img src="artion-logo-white.svg" height="35px" alt="artion logo"/></router-link>
+        </div>
+        <div class="appfooter_item">
+            <a target="_blank" href="https://app.termly.io/document/privacy-policy/7db4b9fc-aa5d-4f80-bfa1-27120ff982ba">
+                Privacy Policy
+            </a>
+        </div>
+        <div class="appfooter_item">
+            <a target="_blank" href="https://app.termly.io/document/cookie-policy/c79f1a78-08a2-4da2-85f0-846a461cde81">
+                Cookie Policy
+            </a>
+        </div>
+        <div class="appfooter_item">
+            <a target="_blank" href="https://app.termly.io/document/terms-of-use-for-online-marketplace/1f69b33f-65ba-40d9-bf63-b28e357f7c34">
+                Terms of Service
+            </a>
+        </div>
+        <div class="appfooter_item">
+            <a target="_blank" href="https://fantom.foundation/?__cf_chl_jschl_tk__=pmd_E1oHTDXhwdz0vcFL0YFXwPOHh90x0.2yXjcuNnnha8E-1632371438-0-gqNtZGzNAdCjcnBszQyl">
+                <img src="fantom_logo.svg" height="35px" alt="fantom logo"/>
+            </a>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'AppFooter',
 };
-</script>
+</script>                                       
 
-<style lang="scss">
+<style lang="scss">                 
 @use "style";
 </style>
