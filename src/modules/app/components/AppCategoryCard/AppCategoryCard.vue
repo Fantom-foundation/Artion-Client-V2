882db634789fb5538154c41e00513f54582a1f6c
sidevshiy <evgeni_lyax7@mail.ru>
2021-09-24 12:12:14 +0200

    Content for AppCategoryList component

--- src/modules/app/components/AppCategoryCard/AppCategoryCard.vue
+++ src/modules/app/components/AppCategoryCard/AppCategoryCard.vue
@@ -1,10 +1,88 @@
 <template>
-    <div class="appcategorycard">AppCategoryCard</div>
+    <div class="appcategorycard">
+        <router-link :to="link" tag="div">
+            <div class="appcategorycard_img">
+                <app-iconset :icon="category.icon || noCategoryData.icon" style="width: 84px; height: 84px;" original />
+            </div>
+            <div class="appcategorycard_label" v-if="!noCategory">
+                <h3>{{ $t(category.label) }}</h3>
+                <svg
+                    width="48"
+                    class="rtl-mirror"
+                    height="48"
+                    viewBox="0 0 48 48"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <rect width="48" height="48" rx="24" fill="#1969FF" />
+                    <path
+                        d="M21 18.0001L27 24.0002L21 30.0002"
+                        stroke="white"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                    />
+                </svg>
+            </div>
+            <div v-else class="appcategorycard_label appcategorycard_label-all">
+                <h3>{{ $t(noCategoryData.label) }}</h3>
+                <svg
+                    width="48"
+                    height="48"
+                    class="rtl-mirror"
+                    viewBox="0 0 48 48"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <rect width="48" height="48" rx="24" fill="white" />
+                    <path
+                        d="M21 18.0001L27 24.0002L21 30.0002"
+                        stroke="#1969FF"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                    />
+                </svg>
+            </div>
+        </router-link>
+    </div>
 </template>
 
 <script>
+import AppIconset from '@/modules/app/components/AppIconset/AppIconset.vue';
 export default {
     name: 'AppCategoryCard',
+    components: { AppIconset },
+    props: {
+        category: {
+            type: Object,
+            default: () => ({
+                id: '',
+                icon: '',
+                label: '',
+            }),
+        },
+        noCategory: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    data() {
+        return {
+            noCategoryData: {
+                icon: 'magnifier',
+                label: this.$t('appsectioncategories.allCategories'),
+            },
+        };
+    },
+
+    computed: {
+        link() {
+            return this.category.id === ''
+                ? { name: 'explore' }
+                : { name: 'explore', query: { category: this.category.id } };
+        },
+    },
 };
 </script>
 
