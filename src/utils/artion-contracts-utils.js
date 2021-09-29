e604f316504d2c5d23ccee971c4212d3f37817d8
HonzaDajc <jan.dajc@gmail.com>
2021-09-29 20:16:34 +0200

    Added function for registering NFT token royalty on Markteplace contract

--- src/utils/artion-contracts-utils.js
+++ src/utils/artion-contracts-utils.js
@@ -1,3 +1,6 @@
+// ZERO_AMOUNT represents zero amount transferred on some calls.
+const ZERO_AMOUNT = '0x0';
+
 /**
  * createNFTCollection Creates a new ERC721 collection contract thru factory
  *
@@ -91,4 +94,102 @@
 export default {
     createNFTCollection,
     createNFT
+}
+
+/**
+ * registerTokenRoyalty Method for setting royalty of NFT token
+ *
+ * @param {string} nftAddress Address of the NFT token
+ * @param {int} tokenID NFT token ID
+ * @param {int} royalty Royalty which is paid to minter of NFT token
+ * @param {Web3} web3Client Instance of an initialized Web3 client.
+ * @return {{to: address, data: string, value string}}
+ */
+ function registerTokenRoyalty(nftAddress, tokenID, royalty, web3Client) {
+
+    const abi = {
+        "inputs": [
+            {
+            "internalType": "address",
+            "name": "_nftAddress",
+            "type": "address"
+            },
+            {
+            "internalType": "uint256",
+            "name": "_tokenId",
+            "type": "uint256"
+            },
+            {
+            "internalType": "uint16",
+            "name": "_royalty",
+            "type": "uint16"
+            }
+        ],
+        "name": "registerRoyalty",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+
+    const encodedAbi = web3Client.eth.abi.encodeFunctionCall(abi,[nftAddress, tokenID, royalty])
+
+    // return tx object
+    return {
+        from: undefined,
+        to: process.env.VUE_APP_FANTOM_MARKETPLACE_CONTRACT_ADDRESS,
+        value: ZERO_AMOUNT,
+        data: encodedAbi,
+    };
+}
+
+/**
+ * registerTokenRoyalty Method for setting royalty of NFT token
+ *
+ * @param {string} nftAddress Address of the NFT token
+ * @param {int} tokenID NFT token ID
+ * @param {int} royalty Royalty which is paid to minter of NFT token
+ * @param {Web3} web3Client Instance of an initialized Web3 client.
+ * @return {{to: address, data: string, value string}}
+ */
+ function registerTokenRoyalty(nftAddress, tokenID, royalty, web3Client) {
+
+    const abi = {
+        "inputs": [
+            {
+            "internalType": "address",
+            "name": "_nftAddress",
+            "type": "address"
+            },
+            {
+            "internalType": "uint256",
+            "name": "_tokenId",
+            "type": "uint256"
+            },
+            {
+            "internalType": "uint16",
+            "name": "_royalty",
+            "type": "uint16"
+            }
+        ],
+        "name": "registerRoyalty",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+
+    const encodedAbi = web3Client.eth.abi.encodeFunctionCall(abi,[nftAddress, tokenID, royalty])
+
+    // return tx object
+    return {
+        from: undefined,
+        to: process.env.VUE_APP_FANTOM_MARKETPLACE_CONTRACT_ADDRESS,
+        value: ZERO_AMOUNT,
+        data: encodedAbi,
+    };
+}
+
+export default {
+    createNFTCollection,
+    createNFT,
+    registerTokenRoyalty
 }
