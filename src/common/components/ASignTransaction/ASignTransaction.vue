fe7be06d234b779300eb750642e46fe7676cccb8
Pavel Spacil <pavel.spacil@gmail.com>
2021-10-14 10:44:24 +0200

    Adjust `ASignTransaction` component

--- src/common/components/ASignTransaction/ASignTransaction.vue
+++ src/common/components/ASignTransaction/ASignTransaction.vue
@@ -4,6 +4,7 @@
 
 <script>
 import { eventBusMixin } from 'fantom-vue-components/src/mixins/event-bus.js';
+import { toBigNumber, toHex } from '@/utils/big-number.js';
 
 export default {
     name: 'ASignTransaction',
@@ -21,6 +22,12 @@ export default {
             type: Boolean,
             default: false,
         },
+    },
+
+    data() {
+        return {
+            status: '',
+        };
     },
 
     watch: {
@@ -44,26 +51,49 @@ export default {
                 this._eventBus.emit('show-wallet-picker');
             } else {
                 try {
+                    this.setStatus('pending');
+
                     if (!tx.from) {
                         tx.from = $wallet.account;
                     }
 
-                    console.log(JSON.stringify(tx));
-
                     tx.chainId = $wallet.chainId;
-                    tx.nonce = await this.$wallet.getNonce($wallet.account);
+                    tx.nonce = await this.$wallet.getNonce($wallet.account, true);
                     // tx.from = $wallet.account;
                     tx.gasLimit = await this.$wallet.estimateGas(tx);
+                    tx.gasPrice = await this.$wallet.getGasPrice(true);
+
+                    tx.gasLimit = toHex(toBigNumber(tx.gasLimit).plus(2000));
+
+                    if (!tx.from) {
+                        tx.from = this.$wallet.account;
+                    }
+
+                    console.log('TX: ', tx);
+
+                    const txHash = await this.$wallet.signTransaction(tx, true);
+
+                    console.log('DONE!!!!');
+
+                    this.setStatus('success', txHash);
                 } catch (error) {
-                    this.$notifications.add({
-                        type: 'error',
-                        text: error.message,
-                    });
+                    this.setStatus('error', error);
                     console.error(error);
                 }
-
-                console.log('tx: ', tx);
             }
+        },
+
+        setStatus(status, data) {
+            this.status = status;
+
+            if (status === 'error') {
+                this.$notifications.add({
+                    type: 'error',
+                    text: data.message,
+                });
+            }
+
+            this.$emit('transaction-status', { status, data });
         },
     },
 };
