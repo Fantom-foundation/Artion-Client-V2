8e7bf52d8d249b9d09819bf76db772b2c1c66e07
sidevshiy <evgeni_lyax7@mail.ru>
2021-10-16 18:19:31 +0200

    Hamberger component + account sidebar fix

---
+++ src/modules/app/components/AppHamburger/AppHamburger.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="burger" :class="{ active: dValue }" @click="toggleBurger"><span></span></div>
+</template>
+<script>
+export default {
+    name: 'AppHamburger',
+    data() {
+        return {
+            dValue: false,
+        };
+    },
+
+    watch: {
+        dValue(_value) {
+            this.$emit('switched', _value);
+        },
+        $route() {
+            this.dValue = false;
+        },
+    },
+
+    methods: {
+        toggleBurger() {
+            this.dValue = !this.dValue;
+        },
+    },
+};
+</script>
+<style lang="scss">
+@use 'style';
+</style>
