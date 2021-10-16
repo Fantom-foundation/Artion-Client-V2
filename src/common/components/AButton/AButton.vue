8cd603ffc39a6ad1598ba29dfb1750923bffadfb
sidevshiy <evgeni_lyax7@mail.ru>
2021-10-16 14:14:37 +0200

    AButton component (spinner)

---
+++ src/common/components/AButton/AButton.vue
@@ -0,0 +1,22 @@
+<template>
+	<f-button v-bind="$attrs" v-on="$listeners" :class="{ 'btn-loading': loading }">
+		<span v-if="loading" class="fbutton_spinner">
+			<div class="spin"></div>
+		</span>
+		<span class="fbutton_content"><slot /></span>
+	</f-button>
+</template>
+<script>
+export default {
+	name: 'AButton',
+	props: {
+		loading: {
+			type: Boolean,
+			default: false,
+		}
+	}
+}
+</script>
+<style lang="scss">
+@use 'style';
+</style>
