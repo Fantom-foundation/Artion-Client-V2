1b3364186865e9d9fd2b2f45b92b936cf3257614
Pavel Spacil <pavel.spacil@gmail.com>
2021-10-19 20:21:32 +0200

    Add `label` prop to `AButton`

--- src/common/components/AButton/AButton.vue
+++ src/common/components/AButton/AButton.vue
@@ -1,9 +1,11 @@
 <template>
-    <f-button v-bind="$attrs" v-on="$listeners" :class="{ 'btn-loading': loading }" :disabled="loading">
+    <f-button v-bind="$attrs" :label="label" v-on="$listeners" :class="{ 'btn-loading': loading }" :disabled="loading">
         <span v-if="loading" class="fbutton_spinner">
             <div class="spin"></div>
         </span>
-        <span class="fbutton_content"><slot /></span>
+        <span class="fbutton_content">
+            <slot>{{ label }}</slot>
+        </span>
     </f-button>
 </template>
 <script>
@@ -13,6 +15,10 @@ export default {
         loading: {
             type: Boolean,
             default: false,
+        },
+        label: {
+            type: String,
+            default: '',
         },
     },
 };
