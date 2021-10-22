ba5b7321f1e605a0c2302aa4eccd208b082ea65b
Lubomír Jahn <lubomir.j@icloud.com>
2021-10-22 10:42:07 +0200

    feat(pg): :sparkles: account settings address form

--- src/modules/pg/views/PGAccountSettings/PGAccountSettings.vue
+++ src/modules/pg/views/PGAccountSettings/PGAccountSettings.vue
@@ -1,14 +1,23 @@
 <template>
-    <div class="pgaccountsettings">PGAccountSettings</div>
+    <div class="pg-account-settings">
+        <h2 class="h2 pg-account-settings__header">{{ $t('pgAccountSettings.accountSettings') }}</h2>
+
+        <div class="pg-account-settings__form">
+            <account-profile-form />
+        </div>
+    </div>
 </template>
 
 <script>
+import AccountProfileForm from '@/modules/account/components/AccountProfileForm/AccountProfileForm.vue';
 import { authPageMixin } from '@/common/mixins/auth-page.js';
 
 export default {
     name: 'PGAccountSettings',
 
     mixins: [authPageMixin],
+
+    components: { AccountProfileForm },
 };
 </script>
 
