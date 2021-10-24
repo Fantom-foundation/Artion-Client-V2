6aeea377e3d27051ca8d53b15c40b99ea6ba5c88
Pavel Spacil <pavel.spacil@gmail.com>
2021-10-24 20:31:57 +0200

    Create helper function `getIPFSUrl`

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,9 +1,21 @@
 import { artionFApolloClient } from '@/plugins/apollo/apollo-provider.js';
 
+/**
+ * @param {string} imageThumbUrl
+ * @return {string}
+ */
 export function getImageThumbUrl(imageThumbUrl) {
     if (!imageThumbUrl) {
         return '';
     }
 
     return `${artionFApolloClient.getCurrentHttpProviderOrigin()}${imageThumbUrl}`;
+}
+
+/**
+ * @param {string} hash
+ * @return {string}
+ */
+export function getIPFSUrl(hash) {
+    return `https://cloudflare-ipfs.com/ipfs/${hash}`;
 }
