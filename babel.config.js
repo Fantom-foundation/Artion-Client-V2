3de89e734d8789941c03b7cb405b1ea893141347
Pavel Spacil <pavel.spacil@gmail.com>
2021-12-16 15:48:03 +0100

    Enable console.log on production (temporary)

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,10 @@
 const plugins = [];
 
+/*
 if (process.env.NODE_ENV === 'production') {
     plugins.push('transform-remove-console');
 }
+*/
 
 module.exports = {
     presets: ['@vue/cli-plugin-babel/preset'],
