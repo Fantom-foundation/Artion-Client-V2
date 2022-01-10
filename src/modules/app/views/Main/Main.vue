e05dc025798795e61c372fd1fffaef081dff7c9c
Pavel Spacil <pavel.spacil@gmail.com>
2022-01-10 10:18:12 +0100

    Add 'Skip to content' navigation link

--- src/modules/app/views/Main/Main.vue
+++ src/modules/app/views/Main/Main.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="main">
+        <f-hidden-nav-link to="main">{{ $t('skipToContent') }}</f-hidden-nav-link>
+
         <app-header />
 
-        <main>
+        <main id="main" tabindex="-1">
             <router-view />
         </main>
     </div>
@@ -10,10 +12,12 @@
 
 <script>
 import AppHeader from '@/modules/app/components/AppHeader/AppHeader.vue';
+import FHiddenNavLink from 'fantom-vue-components/src/components/FHiddenNavLink/FHiddenNavLink.vue';
+
 export default {
     name: 'Main',
 
-    components: { AppHeader },
+    components: { AppHeader, FHiddenNavLink },
 };
 </script>
 
