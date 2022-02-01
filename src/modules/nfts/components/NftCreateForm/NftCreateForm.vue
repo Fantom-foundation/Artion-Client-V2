20aed6767754a278b8aafef1614e5015446127e5
Jan Kalina <jan.kalina@fantom.foundation>
2022-02-01 15:59:47 +0100

    Update default collection address

--- src/modules/nfts/components/NftCreateForm/NftCreateForm.vue
+++ src/modules/nfts/components/NftCreateForm/NftCreateForm.vue
@@ -144,7 +144,7 @@ export default {
     data() {
         return {
             values: {
-                collectionId: '0xf9b895fcdcc1f997df838d5c5b88dafe4d6c1539', // Default Artion Collection
+                collectionId: '0xac7a698a85102f7b1dc7345e7f17ebca74e5a9e7', // Default Artion Collection
             },
             collections: [],
             collection: {},
